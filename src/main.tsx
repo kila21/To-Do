4d83195d07e6fb4780f1f7661eeff299cb0d5e41
kila <luka.kilasonia.1@btu.edu.ge>
2023-04-06 00:01:26 +0400

    add router-dom and main route

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+import Auth from './components/Auth/Auth'
 import './index.css'
+
+const router = createBrowserRouter([
+  {path: '/', element: <Auth />}
+])
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
+
+
