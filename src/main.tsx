fb0f19cc3656a077abc7750322a57edeac15d87e
kila <luka.kilasonia.1@btu.edu.ge>
2023-04-06 12:33:58 +0400

    main types for Store

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import { configureStore } from '@reduxjs/toolkit';
 
-import Auth from './components/Auth/Auth'
+import Auth from './components/auth/Auth';
+import ToDo from './components/todo/ToDo';
+
 import './index.css'
 
+const store = configureStore({
+  reducer: {
+    
+  }
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+
 const router = createBrowserRouter([
-  {path: '/', element: <Auth />}
+  {path: '/', element: <Auth />},
+  {path: '/ToDo', element: <ToDo />}
 ])
 
 
