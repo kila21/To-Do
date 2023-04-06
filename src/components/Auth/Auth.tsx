f67cc881d867aa5d2341a4b2edfe5b8751446e8a
kila <luka.kilasonia.1@btu.edu.ge>
2023-04-06 11:49:55 +0400

    [Auth] i react hooks and implement

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,19 +1,47 @@
+import { useNavigate } from 'react-router-dom';
+import { useForm, SubmitHandler, FieldValue} from 'react-hook-form';
 import './Auth.css'
 
+//import types
+import { AuthInputs } from '../../types/authInputs.type';
+
+
 const Auth = () => {
+    const navigate = useNavigate()
+    const { register, handleSubmit } = useForm()
+    // const onSubmitHandler = () => {
+    //   console.log()
+    // }
+
+    // const onSubmit: SubmitHandler<AuthInputs> = data => console.log(data)
+    const onSubmit: SubmitHandler<AuthInputs> = (data: AuthInputs) => {
+      console.log(data)
+    }
+
     return (
-      <form>
+      //ar vicit ra unda ?????????????????????????????????????????????
+      <form onSubmit={handleSubmit(onSubmit)}>
          <div className="username-container">
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" placeholder='Username'/>
+            <input 
+            id="username" 
+            type="text" 
+            placeholder='Username'
+            {...register('username', {required: true})}
+            />
         </div>
 
         <div className="password-container">
             <label htmlFor="password">Password</label>
-            <input id="password" type="password" placeholder='Password'/>
+            <input 
+            id="password" 
+            type="password" 
+            placeholder='Password'
+            {...register('password', {required: true})}
+            />
         </div>
 
-        <input type='submit' />
+        <input type='submit'/>
       </form>  
     )
 }
